a395db0cc9a44ba6f33295f178893928598f71c0
SunsetFi <sunsetfidev@gmail.com>
2023-08-12 18:42:28 -0700

    xmldoc

--- src/HTTPContextExtensions.cs
+++ src/HTTPContextExtensions.cs
@@ -26,6 +26,12 @@ namespace CSRestAPI
             return JsonConvert.DeserializeObject<T>(text);
         }
 
+        /// <summary>
+        /// Parse the body of the request as a JSON object and deserialize it to the specified type.
+        /// </summary>
+        /// <param name="context">The context to deserialize.</param>
+        /// <param name="type">The type to which the body should be deserialized.</param>
+        /// <returns>The body of the request deserialized to the specified type.</returns>
         public static object ParseBody(this IHttpContext context, Type type)
         {
             var body = context.Request.Body;
@@ -35,6 +41,11 @@ namespace CSRestAPI
             return JsonConvert.DeserializeObject(text, type);
         }
 
+        /// <summary>
+        /// Parse the body of the request and return it as a JSON token.
+        /// </summary>
+        /// <param name="context">The context from which to parse the body.</param>
+        /// <returns>The body of the request as a JToken.</returns>
         public static JToken ParseJson(this IHttpContext context)
         {
             var body = context.Request.Body;
@@ -43,12 +54,27 @@ namespace CSRestAPI
             return JToken.Parse(text);
         }
 
+        /// <summary>
+        /// Send a response with the specified status code and a JSON body.
+        /// </summary>
+        /// <param name="context">The context to which to send the response.</param>
+        /// <param name="statusCode">The HTTP status code of the response.</param>
+        /// <param name="jsonBody">The object to serialize as JSON and include in the body of the response.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
         public static Task SendResponse(this IHttpContext context, HttpStatusCode statusCode, object jsonBody)
         {
             var jsonText = JsonConvert.SerializeObject(jsonBody, Formatting.Indented);
             return SendResponse(context, statusCode, "application/json", jsonText);
         }
 
+        /// <summary>
+        /// Send a response with the specified status code, content type, and body.
+        /// </summary>
+        /// <param name="context">The context to which to send the response.</param>
+        /// <param name="statusCode">The HTTP status code of the response.</param>
+        /// <param name="contentType">The MIME type of the response's content.</param>
+        /// <param name="body">The content to include in the body of the response.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
         public static async Task SendResponse(this IHttpContext context, HttpStatusCode statusCode, string contentType = null, string body = null)
         {
             var response = context.Response;
@@ -66,12 +92,6 @@ namespace CSRestAPI
             }
             else if (body != null)
             {
-                // This is resulting in no data sent.
-                //  What is WriteAllJsonAsync actually doing that makes it json specific?
-                // var stream = new MemoryStream();
-                // var writer = new StreamWriter(stream, System.Text.Encoding.UTF8);
-                // writer.Write(body);
-                // await response.WriteAllAsync(stream, contentType);
                 await response.WriteAllJsonAsync(body);
             }
         }
